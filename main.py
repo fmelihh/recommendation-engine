75d40a1e2c75592c5521e13cc442c526b525b75d
Furkan Melih Ercan <99879453+fmelihh@users.noreply.github.com>
2024-04-19 10:14:28 +0300

    feat: github ci added.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return "hello world"
+
+
 if __name__ == "__main__":
-    print("a")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
