6c01d56ea31139d44572dc2f0e0a4d524fb3b11d
fmelihh <furkanmelihercan.98@gmail.com>
2024-05-19 17:07:34 +0300

    feat: restaurant data class was developed

--- src/recommendation_engine/app/domain/restaurants/values/yemeksepeti/restaurant.py
+++ src/recommendation_engine/app/domain/restaurants/values/yemeksepeti/restaurant.py
@@ -1,8 +1,21 @@
 from dataclasses import dataclass
 
+from ..price import Price
+from ..geo import GeoValue
 from ..restaurant import RestaurantValue
 
 
 @dataclass(frozen=True)
 class YemeksepetiRestaurantValue(RestaurantValue):
-    pass
+    name: str
+    rating: int
+    url_slug: str
+    restaurant_id: str
+    review_number: int
+    coordinates: GeoValue
+    minimum_pickup_time: float
+    minimum_order_amount: Price
+    minimum_delivery_fee: Price
+    minimum_delivery_time: float
+    original_deliveriy_fee: Price
+    loyalty_percentage_amount: float
