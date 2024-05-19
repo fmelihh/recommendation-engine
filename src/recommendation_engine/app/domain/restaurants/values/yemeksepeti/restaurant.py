43aab6b692b5a0b9437c8a87cb23992e7f1d141b
fmelihh <furkanmelihercan.98@gmail.com>
2024-05-19 17:38:36 +0300

    feat: validation method structure was completed

--- src/recommendation_engine/app/domain/restaurants/values/yemeksepeti/restaurant.py
+++ src/recommendation_engine/app/domain/restaurants/values/yemeksepeti/restaurant.py
@@ -14,8 +14,44 @@ class YemeksepetiRestaurantValue(RestaurantValue):
     review_number: int | None
     coordinates: GeoValue | dict | None
     minimum_pickup_time: float | None
-    minimum_order_amount: Price | float
-    minimum_delivery_fee: Price | float
+    minimum_order_amount: Price | float | None
+    minimum_delivery_fee: Price | float | None
     minimum_delivery_time: float | None
-    original_delivery_fee: Price | float
-    loyalty_percentage_amount: float
+    original_delivery_fee: Price | float | None
+    loyalty_percentage_amount: float | None
+
+    def validate_name(self) -> str:
+        pass
+
+    def validate_rating(self) -> int | None:
+        pass
+
+    def validate_url_slug(self) -> str:
+        pass
+
+    def validate_restaurant_id(self) -> str:
+        pass
+
+    def validate_review_number(self) -> int | None:
+        pass
+
+    def validate_coordinates(self) -> GeoValue | None:
+        pass
+
+    def validate_minimum_pickup_time(self) -> float | None:
+        pass
+
+    def validate_minimum_order_amount(self) -> Price | None:
+        pass
+
+    def validate_minimum_delivery_fee(self) -> Price | None:
+        pass
+
+    def validate_minimum_delivery_time(self) -> float | None:
+        pass
+
+    def validate_original_delivery_fee(self) -> Price | None:
+        pass
+
+    def validate_loyalty_percentage_amount(self) -> float | None:
+        pass
