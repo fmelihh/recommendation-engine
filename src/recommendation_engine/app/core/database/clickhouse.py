31724bb8c803dee82ff824fe862d46b054ae1016
fmelihh <furkanmelihercan.98@gmail.com>
2024-07-07 01:35:23 +0300

    feat: added dependencies and created clickhouse connectior and session developments

---
+++ src/recommendation_engine/app/core/database/clickhouse.py
@@ -0,0 +1,31 @@
+from loguru import logger
+from typing import Iterator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine("clickhouse+native://default:@localhost/default", future=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+ClickhouseBase = declarative_base()
+
+
+def get_session() -> Iterator[Session]:
+    db = SessionLocal()
+    try:
+        yield db
+    except Exception as e:
+        logger.exception(str(e))
+        db.rollback()
+    finally:
+        db.close()
+
+
+__all__ = ["get_session", "ClickhouseBase"]
