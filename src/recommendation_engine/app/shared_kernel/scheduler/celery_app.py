c6ed07397d43768d79da83d4f6e3ffac7236cb03
fmelihh <furkanmelihercan.98@gmail.com>
2024-07-18 20:45:08 +0300

    feat: celery tasks were developed.

--- src/recommendation_engine/app/shared_kernel/scheduler/celery_app.py
+++ src/recommendation_engine/app/shared_kernel/scheduler/celery_app.py
@@ -1,8 +1,9 @@
+import os
 from celery import Celery
 
 celery_application = Celery("app")
-celery_application.conf.timezone = "UTC"
-celery_application.conf.broker_url = "redis://localhost:6379/0"
-celery_application.conf.result_backend = "redis://localhost:6379/0"
+celery_application.conf.timezone = os.getenv("CELERY_TIMEZONE")
+celery_application.conf.broker_url = os.getenv("REDIS_HOST")
+celery_application.conf.result_backend = os.getenv("REDIS_PORT")
 
 __all__ = ["celery_application"]
