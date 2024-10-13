55598a9fe2afceb09268aaf687059ae65dfab742
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2024-10-13 21:00:13 +0300

    feat: migrations

---
+++ migrations/versions/de011ed478ca_menu_table.py
@@ -0,0 +1,26 @@
+"""menu table
+
+Revision ID: de011ed478ca
+Revises: 
+Create Date: 2024-10-13 20:59:32.246389
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'de011ed478ca'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
