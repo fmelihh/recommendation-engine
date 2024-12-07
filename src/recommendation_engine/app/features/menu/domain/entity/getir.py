0331dcf2edd35ca42c47aaa57c1d01f1e6655b17
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2024-12-07 17:43:25 +0300

    feat: menu celery task

--- src/recommendation_engine/app/features/menu/domain/entity/getir.py
+++ src/recommendation_engine/app/features/menu/domain/entity/getir.py
@@ -31,7 +31,7 @@ class GetirMenu(BaseEntity, Processor):
         self.restaurant_slug = restaurant_slug
         self.filter_and_search_payload = RequestValue(
             url=(
-                f"https://getir.com/_next/data/vwVOF9DDuYeyM3fGTxCW9/tr/yemekPage"
+                f"https://getir.com/_next/data/YX-h6Yr_VCBb29dea8rdv/tr/yemekPage"
                 f"/restaurants/{self.restaurant_slug}.json?slug={self.restaurant_slug}"
             ),
             method="GET",
