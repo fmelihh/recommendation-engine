dbdc9d63e33932b12be24d2abc8f640c7fd4a6ef
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2024-12-07 18:09:29 +0300

    feat: menu celery task

--- src/recommendation_engine/app/features/menu/db/model.py
+++ src/recommendation_engine/app/features/menu/db/model.py
@@ -2,7 +2,7 @@ import uuid
 import pendulum
 from sqlalchemy import Column
 from clickhouse_sqlalchemy import engines
-from clickhouse_sqlalchemy.types import String, Float, Int32
+from clickhouse_sqlalchemy.types import String, Float, Int32, Nullable
 
 from ....shared_kernel.database.clickhouse import ClickhouseBase
 
@@ -12,7 +12,7 @@ class MenuModel(ClickhouseBase):
 
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
     category = Column(String)
-    product_id = Column(String)
+    product_id = Column(Nullable(String))
     name = Column(String)
     description = Column(String)
     image_url = Column(String)
