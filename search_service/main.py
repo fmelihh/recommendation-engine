c15eff20949a84fb5f2ee0c4f44fb7d44f1f7eb1
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2024-12-08 23:17:29 +0300

    feat: search workflow was added

--- search_service/main.py
+++ search_service/main.py
@@ -1,36 +1,22 @@
 import uvicorn
-from fastapi import FastAPI, UploadFile, File, Depends
+from fastapi import FastAPI, UploadFile, File
 
 from service import SearchService
 
 app = FastAPI()
 
-def convert_bytes_to_dataframe(content: bytes, content_type: str) -> pd.DataFrame:
-    if content_type not in ("text/csv",):
-        raise ValueError("unsupported file type")
-
-    buffer = io.BytesIO(content)
-    df = pd.read_csv(buffer)
-    buffer.close()
-
-    return df
-
 
 @app.get("/search-restaurant")
-def search_restaurant(
-    text: str,
-    page: int = 0,
-    total_page: int = 10
-):
+def search_restaurant(text: str, page: int = 0, total_page: int = 10):
     pass
 
 
 @app.get("restaurant-upload")
 async def restaurant_upload(file: UploadFile = File(...)):
     content = await file.read()
-    df = convert_bytes_to_dataframe(content=content, content_type=file.content_type)
-    search_service.upload_data(df=df)
-    await file.close()
+    SearchService().data_upload(content, file.content_type)
+
+    return {"info": True}
 
 
 if __name__ == "__main__":
--- search_service/service.py
+++ search_service/service.py
@@ -1,10 +1,11 @@
+import io
+import pandas as pd
+from typing import Generator
 from solr import SolrQuery
-from typing import Literal, Any
 
 
 class SearchService:
-    def __init__(self, kind: Literal["restaurant", "comment", "menu"]):
-        self.kind = kind
+    def __init__(self):
         self._solr_query = None
 
     @property
@@ -13,8 +14,26 @@ class SearchService:
             self._solr_query = SolrQuery()
         return self._solr_query
 
+    @staticmethod
+    def _convert_bytes_to_dataframe(
+        content: bytes, content_type: str
+    ) -> Generator[pd.DataFrame, None, None]:
+        if content_type not in ("text/csv",):
+            raise ValueError("unsupported file type")
+
+        buffer = io.BytesIO(content)
+        df = pd.read_csv(buffer, chunksize=300, sep=",", index_col=0)
+        buffer.close()
+
+        for chunk_df in df:
+            yield chunk_df
+
     def search_restaurant(self):
         pass
 
-    def data_upload(self, data: dict[str, Any]):
-        pass
+    def data_upload(self, content: bytes, content_type: str):
+        for chunk_df in self._convert_bytes_to_dataframe(
+            content=content, content_type=content_type
+        ):
+            data = chunk_df.to_dict(orient="records")
+            self.solr_query.data_upload(data=data)
