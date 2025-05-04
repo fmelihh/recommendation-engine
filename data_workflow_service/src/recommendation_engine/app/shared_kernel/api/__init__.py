5f001a98b4cc8d7fd1111f8ba2f2d7881295ee82
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-05-04 20:48:49 +0300

    feat: aggregate data

--- data_workflow_service/src/recommendation_engine/app/shared_kernel/api/__init__.py
+++ data_workflow_service/src/recommendation_engine/app/shared_kernel/api/__init__.py
@@ -16,3 +16,8 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(
     aggregate_domain_router, prefix="/aggregate_domain", tags=["Aggregate Domain"]
 )
+app.include_router(
+    recommendation_aggregate_domain_router,
+    prefix="/recommendation_aggregate",
+    tags=["Recommendation Aggregate Domain"],
+)
